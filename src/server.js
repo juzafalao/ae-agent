1ccd2020c697e19c48ef21aad068030c6048f481
Juliana Zafalao <juzafalao@gmail.com>
2026-05-15 13:48:48 -0300

    feat: landing page da LIA com design dark/cyan e arquitetura visual

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const { processMessage } = require('./agent');
 const { checkConnection } = require('./zapi');
 
+const path = require('path');
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
